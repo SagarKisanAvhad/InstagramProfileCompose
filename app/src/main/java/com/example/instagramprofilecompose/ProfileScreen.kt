973103d4655e41f52e055561caf09943978dc5d7
Sagar <avhad.avhad@gmail.com>
2021-11-07 18:40:25 +0530

    Created seprate file for Profile Screen.

---
+++ app/src/main/java/com/example/instagramprofilecompose/ProfileScreen.kt
@@ -0,0 +1,18 @@
+package com.example.instagramprofilecompose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfileScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
+
+@Composable
+fun TopBar() {
+
+}
