f145763de5d052749dc50df4673cf8b099898f26
Sagar <avhad.avhad@gmail.com>
2021-11-07 20:00:33 +0530

    RoundImage is created and added inside ProfileSection.

--- app/src/main/java/com/example/instagramprofilecompose/ProfileScreen.kt
+++ app/src/main/java/com/example/instagramprofilecompose/ProfileScreen.kt
@@ -1,6 +1,9 @@
 package com.example.instagramprofilecompose
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
@@ -8,7 +11,9 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -66,4 +71,44 @@ Row(
     
 }
     
+}
+
+@Composable
+fun ProfileSection(
+    modifier: Modifier = Modifier
+){
+    Column(modifier = modifier.fillMaxWidth()) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+
+            RoundImage(
+                image = painterResource(id = R.drawable.ic_launcher_background),
+                modifier = Modifier.size(100.dp).weight(3F)
+                )
+        }
+    }
+}
+
+@Composable
+fun RoundImage(
+    image:Painter,
+    modifier: Modifier = Modifier
+){
+    Image(
+        painter = image,
+        contentDescription = "Profile Pic",
+        modifier = modifier
+            .aspectRatio(ratio = 1F, matchHeightConstraintsFirst = true)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape)
+    )
 }
